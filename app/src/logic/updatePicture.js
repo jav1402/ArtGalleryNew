ae090120d862a6df013fa925bfb7ee2b2caa16b1
Pepialarcon <mdafrica24@gmail.com>
2025-12-15 16:59:05 +0100

    salida conflicto rama

--- app/src/logic/updatePicture.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL= import.meta.env.VITE_API_URL || "[http://localhost:3000](http://localhost:3000)";
-async function updatePicture(id, updateData) {
-    return fetch(`${BASE_URL}/picture/${id}`, {
-        method: "PUT",
-        headers: {"Content-"}
-    })
-}
